881f5011d46be961843c8eb399bfd5fe51474bed
VASA-SRAVYA <vasasravya1@gmail.com>
2025-07-24 10:25:17 +0530

    add

--- model_train.py
+++ model_train.py
@@ -1,6 +1,6 @@
 # model_train.py
 
-import pappndas as pd
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
